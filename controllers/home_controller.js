de8d53148773466672b33122756e4b0e1934bf46
Sagar Kori <sagukori2022@gmail.com>
2023-05-20 09:37:09 +0530

    asyn await implemented

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,35 +2,63 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    // .end to print in the page
-    //res.end('<h1>Controller: Hi! </h1>')
+// module.exports.home = function(req,res){
+//     // .end to print in the page
+//     //res.end('<h1>Controller: Hi! </h1>')
 
-    //to render the page from the views
-    // res.render('home',{
-    //     title:'Home',
-    //     message:'Hi from the Home',
-    // });
+//     //to render the page from the views
+//     // res.render('home',{
+//     //     title:'Home',
+//     //     message:'Hi from the Home',
+//     // });
 
-    //post accumulation, populate the user
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+//     //post accumulation, populate the user
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path: 'comments',
+//         populate: {
+//             path: 'user'
+//         }
+//     })
+//     .exec(function(err, posts){
 
-        User.find({},function(err,user){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: user
-            });
+//         User.find({},function(err,user){
+//             return res.render('home', {
+//                 title: "Codeial | Home",
+//                 posts:  posts,
+//                 all_users: user
+//             });
 
+//         });
+//     });
+
+// };
+
+//async await is implemented 
+module.exports.home = async function(req,res){
+
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+    
+        let user = await User.find();
+        
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: user
         });
-    });
 
+    } catch(error) {
+        console.log("error in rendering the posts",error);
+        return ;
+
+    }
 };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,40 +1,49 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
     console.log('Creating the Post!!!');
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    })
-    .then((result)=>{
-        console.log('Post Created!!')
+    try{
+        let post =  await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        })
+
+        console.log('Post Created!!',post);
         return res.redirect('back');
-    })
-    .catch((error)=>{
+
+    } catch(error) {
         console.log('Error in creating the post',error);
         return;
-    })
+    }
 };
 
 //to delete the post
-module.exports.destroy = function(req,res){
-    //find the post 1st
-    Post.findById(req.params.id,function(err,post){
+module.exports.destroy = async function(req,res){
+
+    try{
+        //find the post 1st
+        let post = await Post.findById(req.params.id);
+
         //.id means converting the object inot strings
         if(post.user == req.user.id){
             post.remove();
 
-            Comment.deleteMany({post: req.params.id}, function(error){
-                console.log("Post is deleted",post.content);
-                return res.redirect('back');
-            })
+            await Comment.deleteMany({post: req.params.id}); 
+            console.log("Post is deleted",post.content);
+            return res.redirect('back');
             
         }else{
             console.log("unauthorized user deleting the post");
             return res.redirect('back');
-        }
-    })        
+        } 
+
+    } catch(error) {
+        console.log(error);
+        return;
+    }
+
+      
 };
 
 
